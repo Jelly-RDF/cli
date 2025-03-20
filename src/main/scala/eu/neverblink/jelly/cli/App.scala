0e27d8ce4fe908dfb3fc3e5e118be44a2c82a263
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-03-20 20:29:32 +0100

    Fix AOT
    - Set up a static initializer of the Jena system to avoid issues with race conditions.
    - Added a thingy for Graal to know which classes from Caffeine to compile into the binary. See: https://github.com/ben-manes/caffeine/issues/1840

--- src/main/scala/eu/neverblink/jelly/cli/App.scala
+++ src/main/scala/eu/neverblink/jelly/cli/App.scala
@@ -3,10 +3,14 @@ package eu.neverblink.jelly.cli
 import caseapp.*
 import eu.neverblink.jelly.cli.command.*
 import eu.neverblink.jelly.cli.command.rdf.*
+import org.apache.jena.sys.JenaSystem
 
 /** Main entrypoint.
   */
 object App extends CommandsEntryPoint:
+  // Initialize Jena now to avoid race conditions later
+  JenaSystem.init()
+
   override def progName: String = "jelly-cli"
 
   override def commands: Seq[Command[?]] = Seq(
