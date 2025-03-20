e9d8d4698227262440fec198c652f17e46766663
Karolina-Bogacka <kbogacka766@gmail.com>
2025-03-20 20:39:03 +0100

    Add base functionality and tests (#20)
    * Add base functionality and tests
    
    * Simplify cod
    
    * Correct test error
    
    * Uncommit semanticdb files
    
    * Move to more meaningful part of gitignore
    
    * Move input file to remaining parameters to make it positional
    
    * Move to a stable version of jelly
    
    * Check if lifecycle changes help
    
    * Change jena versions
    
    * Revert "Change jena versions"
    
    This reverts commit 66a7f520135c5df210b843c80955241b82c1bc61.
    
    * Fix AOT
    
    - Set up a static initializer of the Jena system to avoid issues with race conditions.
    - Added a thingy for Graal to know which classes from Caffeine to compile into the binary. See: https://github.com/ben-manes/caffeine/issues/1840
    
    * revert build.sbt change
    
    ---------
    
    Co-authored-by: Karolina Bogacka <karolina@neverblink.eu>
    Co-authored-by: Ostrzyciel <ostrzycielnozyczek@gmail.com>

--- src/main/scala/eu/neverblink/jelly/cli/command/rdf/RdfFromJelly.scala
+++ src/main/scala/eu/neverblink/jelly/cli/command/rdf/RdfFromJelly.scala
@@ -1,16 +1,43 @@
 package eu.neverblink.jelly.cli.command.rdf
 import caseapp.*
 import eu.neverblink.jelly.cli.JellyCommand
+import eu.ostrzyciel.jelly.convert.jena.riot.{JellyLanguage, JellySubsystemLifecycle}
+import org.apache.jena.riot.system.StreamRDFWriter
+import org.apache.jena.riot.{RDFLanguages, RDFParser}
 
-case class FromJellyRdfOptions()
+import java.io.{File, FileInputStream, FileOutputStream, InputStream, OutputStream}
 
-object RdfFromJelly extends JellyCommand[FromJellyRdfOptions]:
+case class RdfFromJellyOptions(
+    @ExtraName("to") outputFile: Option[String] = None,
+)
+
+object RdfFromJelly extends JellyCommand[RdfFromJellyOptions]:
+  override def group = "rdf"
+
   override def names: List[List[String]] = List(
     List("rdf", "from-jelly"),
   )
 
-  override def run(options: FromJellyRdfOptions, remainingArgs: RemainingArgs): Unit =
-    // This is a placeholder for the actual implementation
-    println("rdf from-jelly")
-    println(options)
-    println(remainingArgs)
+  override def run(options: RdfFromJellyOptions, remainingArgs: RemainingArgs): Unit =
+    val inputStream = remainingArgs.remaining.headOption match {
+      case Some(fileName: String) =>
+        FileInputStream(File(fileName))
+      case _ => System.in
+    }
+    val outputStream = options.outputFile match {
+      case Some(fileName: String) =>
+        FileOutputStream(fileName)
+      case None => getStdOut
+    }
+    doConversion(inputStream, outputStream)
+
+  /*
+    This method reads the Jelly file, rewrite it to NQuads and writes it to some output stream
+   * @param inputStream InputStream
+   * @param outputStream OutputStream
+   */
+  private def doConversion(inputStream: InputStream, outputStream: OutputStream): Unit =
+    val mod = JellySubsystemLifecycle()
+    mod.start()
+    val nQuadWriter = StreamRDFWriter.getWriterStream(outputStream, RDFLanguages.NQUADS)
+    RDFParser.source(inputStream).lang(JellyLanguage.JELLY).parse(nQuadWriter)
--- src/test/scala/eu/neverblink/jelly/cli/command/RdfFromJellySpec.scala
+++ src/test/scala/eu/neverblink/jelly/cli/command/RdfFromJellySpec.scala
@@ -1,12 +1,65 @@
 package eu.neverblink.jelly.cli.command
 
+import caseapp.core.{Indexed, RemainingArgs}
 import eu.neverblink.jelly.cli.command.helpers.*
+import eu.neverblink.jelly.cli.command.rdf.*
+import org.apache.jena.riot.RDFLanguages
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class RdfFromJellySpec extends AnyWordSpec with Matchers with CleanUpFilesAfterTest:
+import scala.io.Source
+import scala.util.Using
+
+class RdfFromJellySpec extends AnyWordSpec with Matchers with CleanUpAfterTest:
   "rdf from-jelly command" should {
-    "be able to convert a Jelly file to NTriples" in {
+    "be able to convert a Jelly file to NTriples output stream" in {
       val jellyFile = DataGenHelper.generateJellyFile(3)
+      val nQuadString = DataGenHelper.generateNQuadString(3)
+      val options = RdfFromJellyOptions(outputFile = None)
+      val args = RemainingArgs(indexedRemaining = List(Indexed(jellyFile)), Seq.empty)
+      val (out, err) =
+        RdfFromJelly.runTest(
+          options,
+          args,
+        )
+      val sortedOut = out.split("\n").map(_.trim).sorted
+      val sortedQuads = nQuadString.split("\n").map(_.trim).sorted
+      sortedOut should contain theSameElementsAs sortedQuads
+    }
+    "be able to convert a Jelly stream to NTriples output stream" in {
+      DataGenHelper.generateJellyInputStream(3)
+      val nQuadString = DataGenHelper.generateNQuadString(3)
+      val options = RdfFromJellyOptions(outputFile = None)
+      val (out, err) = RdfFromJelly.runTest(options)
+      val sortedOut = out.split("\n").map(_.trim).sorted
+      val sortedQuads = nQuadString.split("\n").map(_.trim).sorted
+      sortedOut should contain theSameElementsAs sortedQuads
+    }
+    "be able to convert a Jelly file to NTriples file" in {
+      val jellyFile = DataGenHelper.generateJellyFile(3)
+      val args = RemainingArgs(indexedRemaining = List(Indexed(jellyFile)), Seq.empty)
+      val nQuadString = DataGenHelper.generateNQuadString(3)
+      val outputFile = DataGenHelper.generateOutputFile(RDFLanguages.NQUADS)
+      val options = RdfFromJellyOptions(outputFile = Some(outputFile))
+      val (out, err) = RdfFromJelly.runTest(options, args)
+      val sortedOut = Using.resource(Source.fromFile(outputFile)) { content =>
+        content.getLines().toList.map(_.trim).sorted
+      }
+      val sortedQuads = nQuadString.split("\n").map(_.trim).sorted
+      sortedOut should contain theSameElementsAs sortedQuads
+      out.length should be(0)
+    }
+    "be able to convert a Jelly stream to NTriples file" in {
+      DataGenHelper.generateJellyInputStream(3)
+      val outputFile = DataGenHelper.generateOutputFile(RDFLanguages.NQUADS)
+      val nQuadString = DataGenHelper.generateNQuadString(3)
+      val options = RdfFromJellyOptions(outputFile = Some(outputFile))
+      val (out, err) = RdfFromJelly.runTest(options)
+      val sortedOut = Using.resource(Source.fromFile(outputFile)) { content =>
+        content.getLines().toList.map(_.trim).sorted
+      }
+      val sortedQuads = nQuadString.split("\n").map(_.trim).sorted
+      sortedOut should contain theSameElementsAs sortedQuads
+      out.length should be(0)
     }
   }
