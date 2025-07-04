1bf025262e320a6ff86bbb9a5e65f07be5576ef3
niegrzybkowski <kacper.grzymkowski@neverblink.eu>
2025-07-04 08:07:37 +0200

    Add "--version" command test

--- src/test/scala/eu/neverblink/jelly/cli/command/VersionSpec.scala
+++ src/test/scala/eu/neverblink/jelly/cli/command/VersionSpec.scala
@@ -12,3 +12,11 @@ class VersionSpec extends AnyWordSpec, Matchers:
       out should include("Apache Jena")
     }
   }
+  "--version command" should {
+    "print version" in {
+      val (out, err) = Version.runTestCommand(List("--version"))
+      out should startWith("jelly-cli")
+      out should include("Jelly-JVM")
+      out should include("Apache Jena")
+    }
+  }
