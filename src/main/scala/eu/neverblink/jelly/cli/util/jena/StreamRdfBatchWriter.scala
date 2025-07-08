1b040a6cd2301697ebf8b7397394c64ed23cddec
niegrzybkowski <kacper.grzymkowski@neverblink.eu>
2025-07-08 11:47:41 +0200

    Replace Model with Dataset in StreamRdfBatchWriter

--- src/main/scala/eu/neverblink/jelly/cli/util/jena/StreamRdfBatchWriter.scala
+++ src/main/scala/eu/neverblink/jelly/cli/util/jena/StreamRdfBatchWriter.scala
@@ -1,25 +1,30 @@
 package eu.neverblink.jelly.cli.util.jena
 
 import org.apache.jena.riot.system.StreamRDF
-import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.sparql.core.Quad
 import org.apache.jena.graph.Triple
-import org.apache.jena.rdf.model.Model
 import org.apache.jena.riot.system.StreamRDFLib
 import java.io.OutputStream
 import org.apache.jena.riot.Lang
 import org.apache.jena.riot.RDFDataMgr
+import org.apache.jena.query.DatasetFactory
+import org.apache.jena.query.Dataset
 
 /** A StreamRDF implementation that collects everything into a Model. When finishing, formats
   * everything according to the lang, and emits it to the outputStream. This is meant to be a
   * fallback for non-streaming RDF formats, as it requires all data to be loaded in memory.
   */
 class StreamRdfBatchWriter(val outputStream: OutputStream, val lang: Lang) extends StreamRDF:
-  private val model: Model = ModelFactory.createDefaultModel()
-  private val modelStream: StreamRDF = StreamRDFLib.graph(model.getGraph)
-  override def quad(quad: Quad): Unit = modelStream.quad(quad)
-  override def triple(triple: Triple): Unit = modelStream.triple(triple)
-  override def prefix(prefix: String, iri: String): Unit = modelStream.prefix(prefix, iri)
-  override def base(base: String): Unit = modelStream.base(base)
-  override def finish(): Unit = RDFDataMgr.write(outputStream, model, lang)
+  protected val dataset: Dataset = DatasetFactory.create()
+  protected val datasetStream: StreamRDF = StreamRDFLib.dataset(dataset.asDatasetGraph())
+  override def quad(quad: Quad): Unit = datasetStream.quad(quad)
+  override def triple(triple: Triple): Unit = datasetStream.triple(triple)
+  override def prefix(prefix: String, iri: String): Unit = datasetStream.prefix(prefix, iri)
+  override def base(base: String): Unit = datasetStream.base(base)
+  override def finish(): Unit = writeOutput()
   override def start(): Unit = ()
+  def writeOutput(): Unit =
+    if lang == Lang.RDFXML then
+      RDFDataMgr.write(outputStream, dataset.getDefaultModel, lang)
+    else
+      RDFDataMgr.write(outputStream, dataset, lang)
