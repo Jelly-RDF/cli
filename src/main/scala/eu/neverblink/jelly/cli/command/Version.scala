c7370a3f5f8b6143e9ca9e99af2b26c5747eb519
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-08-26 21:04:02 +0200

    --version: add copyright information
    Add info about creator, license, and add a link to the repo.

--- src/main/scala/eu/neverblink/jelly/cli/command/Version.scala
+++ src/main/scala/eu/neverblink/jelly/cli/command/Version.scala
@@ -4,6 +4,8 @@ import caseapp.*
 import eu.neverblink.jelly.cli.*
 import eu.neverblink.jelly.cli.util.jena.JenaSystemOptions
 
+import java.text.SimpleDateFormat
+import java.util.Date
 import scala.util.{Failure, Success}
 
 @HelpMessage(
@@ -22,6 +24,7 @@ object Version extends JellyCommand[VersionOptions]:
   )
 
   override def doRun(options: VersionOptions, remainingArgs: RemainingArgs): Unit =
+    // Print version info
     val jenaV = BuildInfo.libraryDependencies
       .find(_.startsWith("org.apache.jena:jena-core:")).get.split(":")(2)
     val jellyV = BuildInfo.libraryDependencies
@@ -35,6 +38,7 @@ object Version extends JellyCommand[VersionOptions]:
          |JVM         ${System.getProperty("java.vm.name")} ${System.getProperty("java.vm.version")}
          |----------------------------------------------
          |""".stripMargin.trim)
+    // Print feature support info
     reflectionSupported match {
       case Failure(ex) =>
         printLine("[ ] JVM reflection: not supported. Parsing will be slower.")
@@ -44,3 +48,12 @@ object Version extends JellyCommand[VersionOptions]:
         else printLine("    Run with --debug for details.")
       case Success(_) => printLine("[X] JVM reflection: supported. Parsing optimizations enabled.")
     }
+    // Print copyright info
+    val buildYear = new SimpleDateFormat("yyyy").format(Date(BuildInfo.buildTime))
+    printLine(f"""
+         |Copyright (C) $buildYear NeverBlink and contributors.
+         |Licensed under the Apache License, Version 2.0.
+         |For details, see https://www.apache.org/licenses/LICENSE-2.0
+         |This software comes with no warranties and is provided 'as-is'.
+         |Documentation and author list: https://github.com/Jelly-RDF/cli
+         """.stripMargin)
