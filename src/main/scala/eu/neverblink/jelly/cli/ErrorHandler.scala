c6aff429ba508b98ee38ce4d73b70fb5fd90b544
Piotr Sowiński <ostrzycielnozyczek@gmail.com>
2025-09-14 13:00:46 +0200

    Print stack trace for all unknown errors (#219)

--- src/main/scala/eu/neverblink/jelly/cli/ErrorHandler.scala
+++ src/main/scala/eu/neverblink/jelly/cli/ErrorHandler.scala
@@ -9,16 +9,19 @@ object ErrorHandler:
     t match
       case e: CriticalException =>
         command.printLine(f"${e.getMessage}", toStderr = true)
+        printStackTraceIfDebug(command, t)
       case e: Throwable =>
         command.printLine("Unknown error", toStderr = true)
-    printStackTrace(command, t)
+        // Always print stack trace for unknown exceptions,
+        // as otherwise the user has no clue what happened.
+        t.printStackTrace(command.err)
     command.exit(1, t)
 
   /** Print out stack trace or debugging information
     * @param command
     * @param t
     */
-  private def printStackTrace(
+  private def printStackTraceIfDebug(
       command: JellyCommand[?],
       t: Throwable,
   ): Unit =
